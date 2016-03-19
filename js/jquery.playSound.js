8878652cf09dbbbc4a57d5fe8a1351ff4af1edc9
aykim <theandrewkimm@gmail.com>
2016-03-18 23:46:54 -0400

    add some dope tunes

---
+++ js/jquery.playSound.js
@@ -0,0 +1,16 @@
+
+(function($){
+
+  $.extend({
+    playSound: function(){
+      return $(
+        '<audio id = "music" autoplay="autoplay" style="display:none;">'
+          + '<source src="' + arguments[0] + '.mp3" />'
+          + '<source src="' + arguments[0] + '.ogg" />'
+          + '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />'
+        + '</audio>'
+      ).appendTo('body');
+    }
+  });
+
+})(jQuery);
